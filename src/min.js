a369fd42c076d020bb92033e0413e3cb64adfdb8
Randheer Ramesh K <randheerrrk@gmail.com>
2021-08-01 20:12:01 +0530

    Update min.js

--- src/min.js
+++ src/min.js
@@ -2,4 +2,18 @@ const min = arr => {
     return Math.min(...arr);
 }
 
+// const min = arr => {
+// 	if(arr.length == 0) return arr;
+// 	let result = arr[0];
+// 	let index = 1;
+
+// 	const findMin = (arr, index, result) => {
+// 		if(index==arr.length) return result;
+// 		result = Math.min(result, arr[index]);
+// 		return findMin(arr, index+1, result);
+// 	}
+
+// 	return findMin(arr, index, result);
+// }
+
 module.exports = min;
