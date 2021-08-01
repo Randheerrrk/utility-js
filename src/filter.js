1145cd1c8df64b6ba44705b040efae6d0ffb8f8e
Randheer Ramesh K <randheerrrk@gmail.com>
2021-08-01 19:58:38 +0530

    Update filter.js

--- src/filter.js
+++ src/filter.js
@@ -3,4 +3,20 @@ const filter = (arr, callback) => {
     return arr.filter(callback);
 }
 
+// const filter = (arr, callback) => {
+// 	if(arr.length == 0) return arr;
+// 	let resultArray = new Array();
+// 	let index = 0;
+
+// 	const filterArray = (arr, callback, index, resultArray) => {
+// 		if(index==arr.length) return resultArray;
+// 		if(callback(arr[index])) {
+// 			resultArray.push(arr[index]);
+// 		}
+// 		return filterArray(arr, callback, index+1, resultArray);
+// 	}
+	
+// 	return filterArray(arr, callback, index, resultArray);
+// }
+
 module.exports = filter;
