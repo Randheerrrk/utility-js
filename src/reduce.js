5c0514df7f066cad62a1a47d5d929c622011c42d
Randheer Ramesh K <randheerrrk@gmail.com>
2021-08-01 20:15:23 +0530

    Update reduce.js

--- src/reduce.js
+++ src/reduce.js
@@ -8,4 +8,19 @@ const reduce = (arr, reducer, defaultValue=undefined) => {
     }
 }
 
+// const reduce = (arr, callback, initialValue=undefined) => {
+// 	if(arr.length == 0) return initialValue;
+// 	let result = arr[0];
+// 	let index = 1;
+
+// 	const reduceArray = (arr, callback, index, result) => {
+// 		if(index==arr.length) return result;
+// 		result = callback(result, arr[index]);
+// 		return reduceArray(arr, callback, index+1, result);
+// 	}
+
+// 	result = reduceArray(arr, callback, index, result);
+// 	return initialValue ? initialValue+result : result;
+// }
+
 module.exports = reduce;
