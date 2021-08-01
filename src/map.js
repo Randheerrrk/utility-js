026b377de371d403a059568fd46b048691f2ca35
Randheer Ramesh K <randheerrrk@gmail.com>
2021-08-01 20:00:50 +0530

    Update map.js

--- src/map.js
+++ src/map.js
@@ -3,4 +3,16 @@ const map = (arr, fun) => {
 	return arr.map(fun);
 }
 
+// const map = (arr, callback) => {
+// 	if(arr.length == 0) return [];
+// 	let index = 0;
+// 	const mapFunction = (arr, callback, index) => {
+// 		if(index==arr.length) return arr;
+// 		arr[index] = callback(arr[index]);
+// 		return mapFunction(arr, callback, index+1);
+// 	}
+
+// 	return mapFunction(arr, callback, index);
+// }
+
 module.exports = map;
