d7fb7b577b064cae4895d78d2155f531bba82cae
Randheer Ramesh K <randheerrrk@gmail.com>
2021-08-01 20:10:48 +0530

    Update max.js

--- src/max.js
+++ src/max.js
@@ -2,4 +2,18 @@ const max = arr => {
     return Math.max(...arr);
 }
 
+// const max = arr => {
+// 	if(arr.length == 0) return arr;
+//     let result = arr[0]
+// 	let index = 1;
+
+// 	const findMax = (arr, index, result) => {
+// 		if(index==arr.length) return result;
+// 		result = Math.max(result, arr[index]);
+// 		return findMax(arr, index+1, result);
+// 	}
+
+// 	return findMax(arr, index, result);
+// }
+
 module.exports = max;
